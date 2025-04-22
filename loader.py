4b5b420c29a1d246125583d43e6a5d29a023b45d
Rakshaa <rakshaach3@gmail.com>
2025-04-22 21:58:47 +0530

    Fix: updated model path for deployment

--- loader.py
+++ loader.py
@@ -4,7 +4,7 @@ import pandas as pd
 import joblib
 
 # Load the trained model
-model = joblib.load("notebooks/diabetes_model.pkl")
+model = joblib.load("datasets/diabetes_model.pkl")
 
 # Load the dataset
 df = pd.read_csv("datasets/diabetes.csv")
